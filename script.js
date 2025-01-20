dffc0998002f54d1aba3ff6170ca00ba1cf03177
ccoalman <2732529@sd61learn.ca>
2025-01-20 09:26:16 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -155,3 +155,30 @@ window.onload = function () {
     fetchCategories();
     handleFormSubmit();
 };
+
+
+
+
+// install app
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});  
