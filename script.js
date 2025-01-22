52a3a4b671dac47130d96cb07fb085e25cd7864c
ccoalman <2732529@sd61learn.ca>
2025-01-22 09:05:00 -0800

    Update script.js

--- script.js
+++ script.js
@@ -246,3 +246,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 }); 
+
+// load the service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }, function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+}                    
+             
+
